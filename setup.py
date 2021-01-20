593224a7dac00b8f391194f4a453545ad38e65fc
regnat <rg@regnat.ovh>
2021-01-20 15:06:12 +0100

    Statically generate the tlds list
    Because fetching from the network when building isn't allowed by Nix

--- setup.py
+++ setup.py
@@ -5,34 +5,6 @@ from distutils.command.build_py import build_py
 
 from setuptools import setup, find_packages
 
-if sys.version_info.major >= 3:
-    import urllib.request
-
-    r = urllib.request.urlopen('https://data.iana.org/TLD/tlds-alpha-by-domain.txt')
-    assert r.status == 200
-    data = r.read().decode('utf-8').split('\n')
-else:
-    import urllib
-    
-    r = urllib.urlopen('https://data.iana.org/TLD/tlds-alpha-by-domain.txt')
-    assert r.getcode() == 200
-    data = r.read().split('\n')
-    
-version = re.match('^# Version (?P<version>[0-9]+).*$', data[0]).group('version')
-tlds = [i.lower() for i in data[1:] if i and not i.startswith('#')]
-
-
-class build_tld_py(build_py):
-    def run(self):
-        if not self.dry_run:
-            target_dir = os.path.join(self.build_lib, 'tlds')
-            self.mkpath(target_dir)
-
-            with open(os.path.join(target_dir, '_data.py'), 'w') as f:
-                f.write('tld_set = set(%s)\n' % (tlds, ))
-
-        build_py.run(self)
-
 
 setup(name='tlds',
       version=version,
@@ -50,7 +22,6 @@ setup(name='tlds',
       url='https://github.com/kichik/tlds',
       license='MIT',
       packages=['tlds'],
-      cmdclass={'build_py': build_tld_py},
       include_package_data=True,
       zip_safe=True,
       )
---
+++ update.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import sys
+import os
+import re
+
+if sys.version_info.major >= 3:
+    import urllib.request
+
+    r = urllib.request.urlopen('https://data.iana.org/TLD/tlds-alpha-by-domain.txt')
+    assert r.status == 200
+    data = r.read().decode('utf-8').split('\n')
+else:
+    import urllib
+
+    r = urllib.urlopen('https://data.iana.org/TLD/tlds-alpha-by-domain.txt')
+    assert r.getcode() == 200
+    data = r.read().split('\n')
+
+version = re.match('^# Version (?P<version>[0-9]+).*$', data[0]).group('version')
+tlds = [i.lower() for i in data[1:] if i and not i.startswith('#')]
+
+
+target_dir = 'tlds'
+
+with open(os.path.join(target_dir, '_data.py'), 'w') as f:
+    f.write('tld_set = set(%s)\n' % (tlds, ))
